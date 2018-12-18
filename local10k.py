3932568078068cd00ce2bd89fcf72ebbd6e91769
michaelthane <mthane96@gmail.com>
2018-12-17 22:37:19 -0600

    for i in dict
    Signed-off-by: michaelthane <mthane96@gmail.com>

--- local10k.py
+++ local10k.py
@@ -5,8 +5,13 @@ soup = bs4.BeautifulSoup(file, 'html.parser')
 attributeList = soup.select('a')
 
 for i in range(len(attributeList)):
-    print(i)
-    print(attributeList[i])
-    print(attributeList[i].attrs)
-    print(attributeList[i].attrs.values())
+    # print(i)
+    # print(attributeList[i])
+    # print(attributeList[i].attrs)
+    # # print(attributeList[i].attrs[0])
+    # print(attributeList[i].attrs[1])
+    for n in attributeList[i].attrs:
+        # print(attributeList[i].attrs[n])
+        if attributeList[i].attrs[n] == "BALANCE_SHEETS":
+            print(attributeList[i].attrs[n])
 
