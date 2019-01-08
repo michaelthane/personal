344de8997ea1410018276658603331d1f8cafad4
michaelthane <mthane96@gmail.com>
2019-01-07 20:29:22 -0600

    post holiday update
    Signed-off-by: michaelthane <mthane96@gmail.com>

--- edgar.py
+++ edgar.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+import bs4
+import urllib3
+import urllib
 
 browser = webdriver.Chrome()
 browser.get('https://www.sec.gov')
@@ -27,9 +30,55 @@ for i in range(len(inputElements)):
 
 filingSearchButton.click()
 
-documentElements = browser.find_elements_by_id('documentsbutton')
-for i in range(len(documentElements)):
-    print(documentElements[i].get_attribute())
+currentURL = browser.current_url
+
+tempFile = urllib3.PoolManager()
+res = tempFile.request('GET', currentURL)
+soup = bs4.BeautifulSoup(res,'html.parser')
+for link in soup.findAll('a'):
+    print(link)
+    print(link.get('href'))
+# soup = bs4.BeautifulSoup(tempFile)
+
+
+#
+# testvar = browser.find_elements_by_tag_name("td")
+# for i in range(len(testvar)):
+#     print(i)
+#     print(testvar[i].get_attribute("href"))
+    # try:
+    #     print(testvar[i].tag_name)
+    #     print("success")
+    # except "selenium.common.exceptions.StaleElementReferenceException":
+    #     print('yikes')
+    #     # "selenium.common.exceptions.StaleElementReferenceException"
+
+
+
+#print(testvar[0].find_element_by_css_selector('a'))
+#print(testvar)
+# for i in range(len(testvar)):
+#     print(i)
+    # print(testvar[i].get_attribute('href'))
+    # print(testvar[i].find_element_by_css_selector('a'))
+
+    # testvar[i].find_element(
+# testvar = browser.find_element_by_tag_name('td').get_attribute('nowrap')
+
+
+
+
+# documentTable = browser.find_element_by_class_name('tableFile2')
+# aelements = documentTable.findall
+
+# htmlFile = documentTable.
+
+# documentElements = browser.find_element_by_id('documentsbutton')
+# documentElements.
+# print(documentElements.click())
+# documentElements.click()
+# for i in range(len(documentElements)):
+    # print(documentElements[i].get_attribute())
 
 # documentButton.click()
 
