51a3014bffe889143326c587db3076eb0303fe3b
michaelthane <mthane96@gmail.com>
2019-02-11 21:19:08 -0600

    Partial solution to documents button
    Signed-off-by: michaelthane <mthane96@gmail.com>

--- edgar.py
+++ edgar.py
@@ -30,64 +30,12 @@ for i in range(len(inputElements)):
 
 filingSearchButton.click()
 
-currentURL = browser.current_url
-
-tempFile = urllib3.PoolManager()
-res = tempFile.request('GET', currentURL)
-soup = bs4.BeautifulSoup(res,'html.parser')
-for link in soup.findAll('a'):
-    print(link)
-    print(link.get('href'))
-# soup = bs4.BeautifulSoup(tempFile)
+# tableFile = browser.find_element_by_class_name('tableFile2')
 
 
-#
-# testvar = browser.find_elements_by_tag_name("td")
-# for i in range(len(testvar)):
-#     print(i)
-#     print(testvar[i].get_attribute("href"))
-    # try:
-    #     print(testvar[i].tag_name)
-    #     print("success")
-    # except "selenium.common.exceptions.StaleElementReferenceException":
-    #     print('yikes')
-    #     # "selenium.common.exceptions.StaleElementReferenceException"
+# This only works sometimes where the address will print out
+alpha = browser.find_element_by_partial_link_text('Documents')
+browser.get(alpha.get_attribute('href'))
 
-
-
-#print(testvar[0].find_element_by_css_selector('a'))
-#print(testvar)
-# for i in range(len(testvar)):
-#     print(i)
-    # print(testvar[i].get_attribute('href'))
-    # print(testvar[i].find_element_by_css_selector('a'))
-
-    # testvar[i].find_element(
-# testvar = browser.find_element_by_tag_name('td').get_attribute('nowrap')
-
-
-
-
-# documentTable = browser.find_element_by_class_name('tableFile2')
-# aelements = documentTable.findall
-
-# htmlFile = documentTable.
-
-# documentElements = browser.find_element_by_id('documentsbutton')
-# documentElements.
-# print(documentElements.click())
-# documentElements.click()
-# for i in range(len(documentElements)):
-    # print(documentElements[i].get_attribute())
-
-# documentButton.click()
-
-
-# documentElements = browser.find_elements_by_tag_name('a')
-# documentButtonList = []
-# for i in range(len(documentElements)):
-#     print(documentElements[i])
-#     # if documentElements[i].('id') == 'documentsbutton':
-#     #    documentButtonList.append(documentElements[i])
-# documentElements[0].click()
-#documentButtonList[0].click()
+# address = alpha.get_attribute('href')
+# print(address)
