141b3b3fec46ba04093b967d0046bb111742bf13
michaelthane <mthane96@gmail.com>
2019-02-24 15:33:26 -0600

    Acquired most necessary values from MSFT 2018 10k. Need to figure out how to attach date...
    Signed-off-by: michaelthane <mthane96@gmail.com>

--- local10k.py
+++ local10k.py
@@ -9,20 +9,21 @@ def isNumber(string):
     except ValueError:
         return False
 
-
+# Get list of values indicated by analyzingValue
 def getValues(analyzingValue, tags):
     returnList = []
     for elem in tags:
         correctRow = 0
         subTags = elem.select('td')
         for data in subTags:
+            ### Clean string to raw value
+            # Check if the table data text equals the requested value
             if data.text.strip('\n') == analyzingValue:
                 correctRow = 1
                 returnList.append(data.text.strip('\n'))
-                # print(things.text.strip('\n'))
-            if isNumber(data.text.replace(',', '')) and correctRow == 1:
-                returnList.append(data.text.strip('\n').replace(',', ''))
-                # print(things.text.replace(',', ''))
+            if isNumber(data.text.strip('\n').replace(',', '').replace('(', '').replace(')', '')) \
+                    and correctRow == 1:
+                returnList.append(data.text.strip('\n').replace(',', '').replace('(', '').replace(')', ''))
     return returnList
 
 
@@ -33,7 +34,21 @@ soup = bs4.BeautifulSoup(file, 'html.parser')
 
 attributeList = soup.select('tr')
 
-print(getValues('Total liabilities', attributeList))
+print(getValues("Total liabilities", attributeList))
+print(getValues("Total assets", attributeList))
+print(getValues("Long-term debt", attributeList))
+print(getValues("Total stockholders’ equity", attributeList))
+print(getValues("Diluted earnings per share", attributeList))
+print(getValues("Common stock cash dividends paid", attributeList))
+print(getValues("Net income", attributeList))
+print(getValues("Total revenue", attributeList))
+print(getValues("Retained earnings", attributeList))
+print()
+pprint(getValues("Year Ended June 30,", attributeList))
+
+# If it starts with \xa0\xa0, remove it from list.
+# If value is already in list, delete it, else, keep it.
+
 
 # implement code to look for page break
 # only start recording when on balance sheets page, reset when new page break occurs
