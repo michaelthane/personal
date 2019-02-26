27a68a9e2803cc6baaba60b1a4665bdeb4bd4fc9
michaelthane <mthane96@gmail.com>
2019-02-25 21:40:58 -0600

    Features made/added:
    List/Set combo to eliminate duplicates and maintain ordering
    pandas dataframe added
    cleaning string of \xa0 (non breaking space)
    created dictionary:
    key = name
    value = list of values
    
    Signed-off-by: michaelthane <mthane96@gmail.com>

--- local10k.py
+++ local10k.py
@@ -1,6 +1,9 @@
 import bs4
 from pprint import pprint
 import time
+import pandas as pd
+import unidecode
+
 
 def isNumber(string):
     try:
@@ -12,19 +15,29 @@ def isNumber(string):
 # Get list of values indicated by analyzingValue
 def getValues(analyzingValue, tags):
     returnList = []
+    returnSet = set()
     for elem in tags:
         correctRow = 0
         subTags = elem.select('td')
         for data in subTags:
             ### Clean string to raw value
             # Check if the table data text equals the requested value
-            if data.text.strip('\n') == analyzingValue:
+            if data.text.strip('\n') == analyzingValue and data.text.strip('\n') not in returnSet:
                 correctRow = 1
                 returnList.append(data.text.strip('\n'))
-            if isNumber(data.text.strip('\n').replace(',', '').replace('(', '').replace(')', '')) \
+                returnSet.add(data.text.strip('\n'))
+            if isNumber(data.text.strip('\n').replace(',', '').replace('(', '').replace(')', '')
+                            .replace(u'\xa0', '')) \
+                    and isNumber(data.text.strip('\n').replace(',', '').replace('(', '').replace(')', '')
+                                     .replace(u'\xa0', '')) not in returnSet\
                     and correctRow == 1:
-                returnList.append(data.text.strip('\n').replace(',', '').replace('(', '').replace(')', ''))
+                returnList.append(data.text.strip('\n').replace(',', '').replace('(', '').replace(')', '')
+                                  .replace(u'\xa0', ''))
+                returnSet.add(data.text.strip('\n').replace(',', '').replace('(', '').replace(')', '')
+                                  .replace(u'\xa0', ''))
     return returnList
+
+# def filter():
 
 
 file = open('Companies/MSFT/msft-10k_20180630.htm')
@@ -34,40 +47,63 @@ soup = bs4.BeautifulSoup(file, 'html.parser')
 
 attributeList = soup.select('tr')
 
-print(getValues("Total liabilities", attributeList))
-print(getValues("Total assets", attributeList))
-print(getValues("Long-term debt", attributeList))
-print(getValues("Total stockholders’ equity", attributeList))
-print(getValues("Diluted earnings per share", attributeList))
-print(getValues("Common stock cash dividends paid", attributeList))
-print(getValues("Net income", attributeList))
-print(getValues("Total revenue", attributeList))
-print(getValues("Retained earnings", attributeList))
-print()
-pprint(getValues("Year Ended June 30,", attributeList))
+liabilities = getValues("Total liabilities", attributeList)
+assets = getValues("Total assets", attributeList)
+debt = getValues("Long-term debt", attributeList)
+equity = getValues("Total stockholders’ equity", attributeList)
+dilutedEPS = getValues("Diluted earnings per share", attributeList)
+dividends = getValues("Common stock cash dividends paid", attributeList)
+netIncome = getValues("Net income", attributeList)
+totalRevenue = getValues("Total revenue", attributeList)
+retainedEarnings = getValues("Retained earnings", attributeList)
+
+# print(liabilities)
+# print(assets)
+# print(debt)
+# print(equity)
+# print(dilutedEPS)
+# print(dividends)
+# print(netIncome)
+# print(totalRevenue)
+# print(retainedEarnings)
+# print()
+# pprint(getValues("Year Ended June 30,", attributeList))
+
+listOfLists = [liabilities,
+               assets,
+               debt,
+               equity,
+               dilutedEPS,
+               dividends,
+               netIncome,
+               totalRevenue,
+               retainedEarnings]
+
+longestLen = 0
+for thisList in listOfLists:
+    if len(thisList[1:]) > longestLen:
+        longestLen = len(thisList[1:])
+
+for thisList in listOfLists:
+    if len(thisList[1:]) < longestLen:
+        for i in range(longestLen - len(thisList[1:])):
+            thisList.append(0)
+
+valueDict = {}
+for thisList in listOfLists:
+    valueDict[thisList[0]] = thisList[1:]
+
+#print(valueDict)
+
+df = pd.DataFrame(data=valueDict)
+
+pd.set_option('display.max_columns', 20)
+
+print(df)
+
 
 # If it starts with \xa0\xa0, remove it from list.
 # If value is already in list, delete it, else, keep it.
 
-
 # implement code to look for page break
 # only start recording when on balance sheets page, reset when new page break occurs
-
-
-# returnList = []
-# reportValue = 'Total liabilities'
-# for elem in attributeList:
-#     correctRow = 0
-#     data = elem.select('td')
-#     for things in data:
-#         if things.text.strip('\n') == reportValue:
-#             correctRow = 1
-#             returnList.append(things.text.strip('\n'))
-#             # print(things.text.strip('\n'))
-#         if isNumber(things.text.replace(',', '')) and correctRow == 1:
-#             returnList.append(things.text.replace(',', ''))
-#             #print(things.text.replace(',', ''))
-# print(len(attributeList))
-
-
-
